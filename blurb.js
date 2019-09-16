795dc01a17611f6e797e6210062501ffe70e8b6b
Ashton Snelgrove <ashton@snelgrove.science>
2019-09-16 16:08:01 -0600

    Move blurb to own file

---
+++ blurb.js
@@ -0,0 +1,7 @@
+import { h, render } from 'preact';
+
+const Blurb = () => (
+  <div className="container" style="text-align:center">
+    Designed by <a href="http://siementerpstra.com/" target="new">Siemen Terpstra</a> in the late ’80’s. WebApp developed by <a href="http://jamesfenn.com/" target="new">James Fenn</a> with additions and modifications by <a href="http://brandlew.com/" target="new">Brandon Lewis</a>, <a href="http://whatmusicreallyis.com/" title="What Music Really İs" target="new">Bo Constantinsen</a> and <a href="https://sites.google.com/site/wangchengu/" target="new">Chengu Wang</a>. Credits to Scott Thompson and <a href="http://ozanyarman.com/" target="new">Dr Ozan Yarman</a> for contributing samples. Current version 1.4 (Jan. 2015 — Mar. 2018), released as Free/Libre and Open Source Software under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="new">GPL-3.0</a>. Download, fork, and get your name down here by fixing issues and implementing features via <a href="https://github.com/wcgbg/terpstrakeyboard/" target="new">GitHub</a>!
+  </div>);
+export default Blurb;
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import "./skeleton.css";
 // import "skeleton-css/css/skeleton.css";
 import "./terpstra-style.css"
 import spacer from './1x1.png';
+import Blurb from './blurb';
 
 const findPreset = (preset) => {
   for (let g of presets) {
@@ -129,9 +130,5 @@ const App = () => {
   };
 };
 
-const Blurb = () => (
-  <div className="container" style="text-align:center">
-    Designed by <a href="http://siementerpstra.com/" target="new">Siemen Terpstra</a> in the late ’80’s. WebApp developed by <a href="http://jamesfenn.com/" target="new">James Fenn</a> with additions and modifications by <a href="http://brandlew.com/" target="new">Brandon Lewis</a>, <a href="http://whatmusicreallyis.com/" title="What Music Really İs" target="new">Bo Constantinsen</a> and <a href="https://sites.google.com/site/wangchengu/" target="new">Chengu Wang</a>. Credits to Scott Thompson and <a href="http://ozanyarman.com/" target="new">Dr Ozan Yarman</a> for contributing samples. Current version 1.4 (Jan. 2015 — Mar. 2018), released as Free/Libre and Open Source Software under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="new">GPL-3.0</a>. Download, fork, and get your name down here by fixing issues and implementing features via <a href="https://github.com/wcgbg/terpstrakeyboard/" target="new">GitHub</a>!
-  </div>);
 
 render(<App />, document.getElementById('application'));
