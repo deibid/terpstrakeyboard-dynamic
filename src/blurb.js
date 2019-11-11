83c7864cf2cbeeaf7778cd633f5e8c1d9ab26b04
Ashton Snelgrove <ashton@snelgrove.science>
2019-11-11 09:36:29 -0700

    Update formatting on blurb

--- src/blurb.js
+++ src/blurb.js
@@ -1,7 +1,18 @@
 import { h, render } from 'preact';
 
 const Blurb = () => (
-  <div className="container" style="text-align:center">
-    Designed by <a href="http://siementerpstra.com/" target="new">Siemen Terpstra</a> in the late ’80’s. WebApp developed by <a href="http://jamesfenn.com/" target="new">James Fenn</a> with additions and modifications by <a href="http://brandlew.com/" target="new">Brandon Lewis</a>, <a href="http://whatmusicreallyis.com/" title="What Music Really İs" target="new">Bo Constantinsen</a> and <a href="https://sites.google.com/site/wangchengu/" target="new">Chengu Wang</a>. Credits to Scott Thompson and <a href="http://ozanyarman.com/" target="new">Dr Ozan Yarman</a> for contributing samples. Current version 1.4 (Jan. 2015 — Mar. 2018), released as Free/Libre and Open Source Software under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="new">GPL-3.0</a>. Download, fork, and get your name down here by fixing issues and implementing features via <a href="https://github.com/wcgbg/terpstrakeyboard/" target="new">GitHub</a>!
+  <div>
+    <p>
+      Designed by <a href="http://siementerpstra.com/" target="new">Siemen Terpstra</a> in the late ’80’s.
+    </p>
+    <p>
+      WebApp developed by <a href="http://jamesfenn.com/" target="new">James Fenn</a> with additions and modifications by <a href="http://brandlew.com/" target="new">Brandon Lewis</a>, <a href="http://whatmusicreallyis.com/" title="What Music Really İs" target="new">Bo Constantinsen</a>, <a href="https://sites.google.com/site/wangchengu/" target="new">Chengu Wang</a>, and <a href="https://ashton.snelgrove.science">Ashton Snelgrove</a>.
+    </p>
+    <p>
+      Credits to Scott Thompson and <a href="http://ozanyarman.com/" target="new">Dr. Ozan Yarman</a> for contributing samples.
+    </p>
+    <p>
+      Current version 2.0 (Jan. 2015 — Dec. 2019), released as Free/Libre and Open Source Software under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="new">GPL-3.0</a>. Download, fork, and get your name down here by fixing issues and implementing features via <a href="https://github.com/wcgbg/terpstrakeyboard/" target="new">GitHub</a>!
+    </p>
   </div>);
 export default Blurb;
